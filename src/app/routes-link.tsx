bc110fbbb23c64289443af2347022d75ede03423
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2025-02-25 22:43:57 +0500

    added deals and leads dashboards

--- src/app/routes-link.tsx
+++ src/app/routes-link.tsx
@@ -3,6 +3,8 @@ import Leads from "../features/leads";
 import {Route} from "react-router-dom";
 import Deals from "../features/deals";
 import Contacts from "../features/contacts";
+import DealsDashboard from "../features/deals/deals-dashboard";
+import LeadsDashboard from "../features/leads/leads-dashboard";
 
 export const publicRoutes = [
     {
@@ -22,5 +24,17 @@ export const publicRoutes = [
         element: <Contacts/>,
         route: Route,
         title: "Contacts"
-    }
+    },
+    {
+        path: allroutes.dealsDashboard,
+        element: <DealsDashboard/>,
+        route: Route,
+        title: "Deals Dashboard"
+    },
+    {
+        path: allroutes.leadsDashboard,
+        element: <LeadsDashboard/>,
+        route: Route,
+        title: "Leads Dashboard"
+    },
 ]
